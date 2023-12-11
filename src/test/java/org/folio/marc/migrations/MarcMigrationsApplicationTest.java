34b9010fef086983c16bce37bd257405f8e2a5d3
psmagin <pavlo_smahin@epam.com>
2023-12-11 13:21:42 +0200

    build: fix liquibase upgrade
    Closes: MODMARCMIG-4

--- src/test/java/org/folio/marc/migrations/MarcMigrationsApplicationTest.java
+++ src/test/java/org/folio/marc/migrations/MarcMigrationsApplicationTest.java
@@ -1,11 +1,14 @@
 package org.folio.marc.migrations;
 
+import static org.folio.support.TestConstants.TENANT_ID;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import lombok.SneakyThrows;
+import org.folio.spring.integration.XOkapiHeaders;
 import org.folio.spring.test.extension.EnablePostgres;
 import org.folio.spring.test.type.IntegrationTest;
 import org.junit.jupiter.api.Test;
@@ -38,4 +41,15 @@ class MarcMigrationsApplicationTest {
         .andExpect(status().is2xxSuccessful())
         .andExpect(jsonPath("$.status", equalTo("UP")));
   }
+
+  @Test
+  @SneakyThrows
+  void postTenantEndpointWorks() {
+    mockMvc.perform(MockMvcRequestBuilders.post("/_/tenant")
+            .contentType(APPLICATION_JSON)
+            .header(XOkapiHeaders.TENANT, TENANT_ID)
+            .content("{\"module_to\": \"mod-1.0.0\"}"))
+        .andExpect(status().is2xxSuccessful());
+  }
+
 }
