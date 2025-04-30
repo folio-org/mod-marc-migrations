fe3ad41355568077a8ba2bedc904f1038b02be1d
Mukhiddin Yusupov <mukhiddin_yusupov@epam.com>
2025-04-30 16:58:20 +0500

    fix(migration): Fix Marc Migration APIs error responses
    Closes: MODMARCMIG-51

--- src/main/java/org/folio/marc/migrations/controllers/ApiErrorHandler.java
+++ src/main/java/org/folio/marc/migrations/controllers/ApiErrorHandler.java
@@ -4,17 +4,24 @@ import static java.util.Collections.emptyList;
 import static org.apache.logging.log4j.Level.DEBUG;
 import static org.apache.logging.log4j.Level.TRACE;
 import static org.apache.logging.log4j.Level.WARN;
+import static org.folio.marc.migrations.domain.entities.types.ErrorCode.NOT_FOUND_ERROR;
+import static org.folio.marc.migrations.domain.entities.types.ErrorCode.SERVICE_ERROR;
+import static org.folio.marc.migrations.domain.entities.types.ErrorCode.VALIDATION_ERROR;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
+import jakarta.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.Optional;
 import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.Level;
+import org.folio.marc.migrations.domain.entities.types.ErrorCode;
 import org.folio.marc.migrations.exceptions.ApiValidationException;
 import org.folio.spring.exception.NotFoundException;
 import org.folio.tenant.domain.dto.Error;
+import org.folio.tenant.domain.dto.Errors;
 import org.folio.tenant.domain.dto.Parameter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,37 +38,42 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 public class ApiErrorHandler {
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<Error> handleGlobalExceptions(Exception e) {
+  public ResponseEntity<Errors> handleGlobalExceptions(Exception e) {
     logException(WARN, e);
-    return buildUnprocessableResponseEntity(e, INTERNAL_SERVER_ERROR);
+    return buildResponseEntity(e, INTERNAL_SERVER_ERROR, SERVICE_ERROR);
   }
 
   @ExceptionHandler(NotFoundException.class)
-  public ResponseEntity<Error> handleNotFoundExceptions(NotFoundException e) {
+  public ResponseEntity<Errors> handleNotFoundExceptions(NotFoundException e) {
     logException(TRACE, e);
-    return buildUnprocessableResponseEntity(e, NOT_FOUND);
+    return buildResponseEntity(e, NOT_FOUND, NOT_FOUND_ERROR);
   }
 
   @ExceptionHandler(ApiValidationException.class)
-  public ResponseEntity<Error> handleApiValidationException(ApiValidationException e) {
+  public ResponseEntity<Errors> handleApiValidationException(ApiValidationException e) {
     var parameter = new Parameter(e.getFieldName()).value(e.getFieldValue());
-    return buildUnprocessableResponseEntity(buildError(e, List.of(parameter)));
+    var errorResponse = new Errors();
+    errorResponse.addErrorsItem(buildError(e, VALIDATION_ERROR, List.of(parameter)));
+    errorResponse.setTotalRecords(1);
+    return buildResponseEntity(errorResponse, UNPROCESSABLE_ENTITY);
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<Error> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+  public ResponseEntity<Errors> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
     logException(DEBUG, e);
-    var errors = Optional.of(e.getBindingResult())
+    var errorResponse = new Errors();
+    Optional.of(e.getBindingResult())
       .map(org.springframework.validation.Errors::getAllErrors)
       .orElse(emptyList())
-      .stream()
-      .map(error -> new Error(error.getDefaultMessage())
-        .type(MethodArgumentNotValidException.class.getSimpleName())
-        .addParametersItem(new Parameter(((FieldError) error).getField())
-          .value(String.valueOf(((FieldError) error).getRejectedValue()))))
-      .toList();
-
-    return buildUnprocessableResponseEntity(errors.getFirst());
+        .forEach(error ->
+          errorResponse.addErrorsItem(new Error(error.getDefaultMessage())
+            .message(error.getDefaultMessage())
+            .type(MethodArgumentNotValidException.class.getSimpleName())
+            .code(VALIDATION_ERROR.getValue())
+            .addParametersItem(new Parameter(((FieldError) error).getField())
+              .value(String.valueOf(((FieldError) error).getRejectedValue())))));
+    errorResponse.totalRecords(errorResponse.getErrors().size());
+    return buildResponseEntity(errorResponse, UNPROCESSABLE_ENTITY);
   }
 
   @ExceptionHandler({
@@ -69,13 +81,13 @@ public class ApiErrorHandler {
     MissingServletRequestParameterException.class,
     IllegalArgumentException.class
   })
-  public ResponseEntity<Error> handleValidationException(Exception e) {
+  public ResponseEntity<Errors> handleValidationException(Exception e) {
     logException(DEBUG, e);
-    return buildUnprocessableResponseEntity(e, BAD_REQUEST);
+    return buildResponseEntity(e, BAD_REQUEST, VALIDATION_ERROR);
   }
 
   @ExceptionHandler(HttpMessageNotReadableException.class)
-  public ResponseEntity<Error> handlerHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+  public ResponseEntity<Errors> handlerHttpMessageNotReadableException(HttpMessageNotReadableException e) {
     return Optional.ofNullable(e.getCause())
       .map(Throwable::getCause)
       .filter(IllegalArgumentException.class::isInstance)
@@ -83,24 +95,51 @@ public class ApiErrorHandler {
       .map(this::handleValidationException)
       .orElseGet(() -> {
         logException(DEBUG, e);
-        return buildUnprocessableResponseEntity(e, BAD_REQUEST);
+        return buildResponseEntity(e, BAD_REQUEST, VALIDATION_ERROR);
       });
   }
 
-  private static ResponseEntity<Error> buildUnprocessableResponseEntity(Exception exception, HttpStatus status) {
-    return ResponseEntity.status(status).body(buildError(exception, emptyList()));
-  }
+  /**
+   * Catches and handles all exceptions of type {@link ConstraintViolationException}.
+   *
+   * @param exception {@link ConstraintViolationException} to process
+   * @return {@link ResponseEntity} with {@link Errors} body
+   */
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<Errors> handleConstraintViolation(ConstraintViolationException exception) {
+    logException(DEBUG, exception);
+    var errorResponse = new Errors();
+    exception.getConstraintViolations().forEach(constraintViolation ->
+        errorResponse.addErrorsItem(new Error()
+            .message(String.format("%s %s", constraintViolation.getPropertyPath(), constraintViolation.getMessage()))
+            .code(VALIDATION_ERROR.getValue())
+            .type(ConstraintViolationException.class.getSimpleName())));
+    errorResponse.totalRecords(errorResponse.getErrors().size());
 
-  private static ResponseEntity<Error> buildUnprocessableResponseEntity(Error errorResponse) {
-    return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(errorResponse);
+    return buildResponseEntity(errorResponse, BAD_REQUEST);
   }
 
   private static void logException(Level logLevel, Exception e) {
     log.log(logLevel, "Handling exception", e);
   }
 
-  private static Error buildError(Exception e, List<Parameter> parameters) {
+  private static ResponseEntity<Errors> buildResponseEntity(Exception e, HttpStatus status, ErrorCode code) {
+    var errorResponse = new Errors()
+        .errors(List.of(new Error()
+            .message(e.getMessage())
+            .type(e.getClass().getSimpleName())
+            .code(code.getValue())))
+        .totalRecords(1);
+    return buildResponseEntity(errorResponse, status);
+  }
+
+  private static ResponseEntity<Errors> buildResponseEntity(Errors errorResponse, HttpStatus status) {
+    return ResponseEntity.status(status).body(errorResponse);
+  }
+
+  private static Error buildError(Exception e, ErrorCode code, List<Parameter> parameters) {
     return new Error(e.getMessage())
+      .code(code.getValue())
       .type(e.getClass().getSimpleName())
       .parameters(parameters);
   }
---
+++ src/main/java/org/folio/marc/migrations/domain/entities/types/ErrorCode.java
@@ -0,0 +1,19 @@
+package org.folio.marc.migrations.domain.entities.types;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+  UNKNOWN_ERROR("unknown_error"),
+  SERVICE_ERROR("service_error"),
+  VALIDATION_ERROR("validation_error"),
+  NOT_FOUND_ERROR("not_found_error"),
+  CONSTRAINT_VIOLATION("constraint_violation_error");
+
+  @JsonValue
+  private final String value;
+}
