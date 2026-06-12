aaf2a8b85861dc5d6a240a3bc825e4df1bf15d67
viacheslav_kolesnyk <Viacheslav_Kolesnyk@epam.com>
2026-06-12 18:11:10 +0200

    - Remove custom datasource config - Set spring batch jdbc repositpry isolation level to read_committed

--- src/main/java/org/folio/marc/migrations/MarcMigrationsApplication.java
+++ src/main/java/org/folio/marc/migrations/MarcMigrationsApplication.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Isolation;
 @EnableRetry
 @EnableCaching
 @EnableBatchProcessing
-@EnableJdbcJobRepository(isolationLevelForCreate = Isolation.SERIALIZABLE)
+@EnableJdbcJobRepository(isolationLevelForCreate = Isolation.READ_COMMITTED)
 @SpringBootApplication
 public class MarcMigrationsApplication {
 
